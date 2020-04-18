1844c8ce3fba465930027bd706b898bbf8608c00
Carlos <carlos@ticloop.com>
2020-04-18 14:23:49 +0200

    Init commit

---
+++ precache-manifest.a8ee7c6a40d6ade8f83ae4352e7788f0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "c97d238339edea4c33afd5a7f6a0d8ce",
+    "url": "./assets/images/icon-512px.png"
+  },
+  {
+    "revision": "44bb40dbe9cc551a3ed3",
+    "url": "./js/bundle.js"
+  },
+  {
+    "revision": "3f81c3c6934d1710503e",
+    "url": "./js/vendor.bundle.js"
+  },
+  {
+    "revision": "92346e5c2a260b93e5b7e744c5981a82",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f8ba06c323a8fc17c0044df6df948a1f",
+    "url": "./assets/images/phaser-es6-webpack.jpg"
+  },
+  {
+    "revision": "bfd7f2fb9488329e9f3e8ca1993118ca",
+    "url": "./assets/images/mushroom2.png"
+  },
+  {
+    "revision": "19020c7cd6cd0a738009e0ffbdc3940f",
+    "url": "./assets/images/machupichu.png"
+  },
+  {
+    "revision": "a011b2642743fd2f4ee5649e9a382014",
+    "url": "./assets/images/machupichu.jpg"
+  },
+  {
+    "revision": "dfb749fb41edc5c53626b50ec9d48776",
+    "url": "./assets/images/loader-bg.png"
+  },
+  {
+    "revision": "78ea0555ef7648bf378481a5d778d344",
+    "url": "./assets/images/loader-bar.png"
+  },
+  {
+    "revision": "6eb0c377e0ea2aac41ac8d8b229a8020",
+    "url": "./assets/images/icon-192px.png"
+  },
+  {
+    "revision": "5ad3e9d6a2b3ce664b1bf3ed46c7d155",
+    "url": "./assets/.DS_Store"
+  },
+  {
+    "revision": "3585bc041684e8845b639088608b3bb2",
+    "url": "././manifest.json"
+  }
+];
